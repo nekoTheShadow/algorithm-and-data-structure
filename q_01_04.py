5a75e0ebcaf878b45785279c2808fc89443762a8
hajime nakamura <h.nakamura0903@gmail.com>
2020-11-22 23:48:16 +0900

    章末問題1.4

---
+++ q_01_04.py
@@ -0,0 +1,24 @@
+import mushikui
+
+question = """
+    xxxxxx
+      xxxx
+----------
+    66xxxx
+   6xxxxx 
+ xx666xx
+ xx6xx6
+----------
+xxxx66xxxx
+"""
+
+mushikui.solve(question)
+#     666666
+#       1711
+# ----------
+#     666666
+#    666666 
+#  4666662
+#  666666
+# ----------
+# 1140665526
