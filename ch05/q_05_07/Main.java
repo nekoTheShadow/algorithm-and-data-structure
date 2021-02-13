4f7ddb228ea3ad6b45c4e78214be3a524eb5994a
hajime nakamura <h.nakamura0903@gmail.com>
2021-02-13 15:05:41 +0900

    章末問題5.7

---
+++ ch05/q_05_07/Main.java
@@ -0,0 +1,224 @@
+package ch05.q_05_07;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.lang.reflect.Array;
+import java.util.Objects;
+
+
+/**
+ * https://atcoder.jp/contests/dp/submissions/20135978
+ */
+public class Main {
+    public void exec() {
+        String s = stdin.nextString();
+        String t = stdin.nextString();
+        stdout.println(lcs(s, t));
+    }
+
+    public String lcs(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n+1][m+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        int x = n;
+        int y = m;
+        StringBuilder u = new StringBuilder();
+        while(x > 0 && y > 0) {
+            if (dp[x][y] == dp[x-1][y]) {
+                x--;
+            } else if (dp[x][y] == dp[x][y-1]) {
+                y--;
+            } else {
+                x--;
+                y--;
+                u.append(s.charAt(x));
+            }
+        }
+
+        return u.reverse().toString();
+    }
+
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+    private static final Stderr stderr = new Stderr(System.err, false);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    // ASCII ONLY
+    public static class Stdin {
+        private InputStream in;
+        private byte[] buf;
+        private int ptr;
+        private int len;
+
+        public Stdin(InputStream in) {
+            this.in = in;
+            this.buf = new byte[1024];
+            this.ptr = 0;
+            this.len = 0;
+        }
+
+        public String nextString() {
+            StringBuilder sb = new StringBuilder();
+            byte b;
+            while ((b = read()) != -1) {
+                sb.appendCodePoint(b);
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() {
+            return (int)nextLong();
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            boolean negative = false;
+            int x = 0;
+
+            byte b = read();
+            if (b == '-') {
+                negative = true;
+            } else {
+                x += b-'0';
+            }
+
+            while ((b=read()) != -1) {
+                x *= 10;
+                x += b-'0';
+            }
+
+            return negative ? -x : x;
+        }
+
+        private byte read() {
+            byte b = readByte();
+            if (b == '\r') {
+                readByte(); // LFを読み飛ばす
+                return -1;
+            } else if (b == '\n' || b == ' ') {
+                return -1;
+            } else {
+                return b;
+            }
+        }
+
+        private byte readByte(){
+            if (len == ptr) {
+                try {
+                    ptr = 0;
+                    len = in.read(buf);
+                    if (len == -1) return -1;
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return buf[ptr++];
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) a[i] = nextString();
+            return a;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        public double[] nextDoubleArray(int n) {
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++) a[i] = nextDouble();
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+
+    public static class Stderr {
+        private PrintWriter stderr;
+        private boolean debug;
+
+        public Stderr(PrintStream stderr, boolean debug) {
+            this.stderr =  new PrintWriter(stderr, false);
+            this.debug = debug;
+        }
+
+        public void println(Object ... objs) {
+            if (!debug) return ;
+
+            stderr.print("DEBUG: ");
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stderr.print(deepToString(objs[i]));
+                if (i != len-1) stderr.print(' ');
+            }
+            stderr.println();
+            stderr.flush();
+        }
+
+        private String deepToString(Object o) {
+            if (o == null) {
+                return "null";
+            }
+
+            // 配列の場合
+            if (o.getClass().isArray()) {
+                int len = Array.getLength(o);
+                String[] tokens = new String[len];
+                for (int i = 0; i < len; i++) {
+                    tokens[i] = deepToString(Array.get(o, i));
+                }
+                return "{" + String.join(", ", tokens) + "}";
+            }
+
+            return Objects.toString(o);
+        }
+    }
+}
